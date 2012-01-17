4a2ea3d2842d48a5cda1c6e54f4d250b8f3682b4
Peter Thiem <peterthiem@gmail.com>
2012-01-17 13:43:16 +1100

    Third commit

---
+++ Release/bin/make.bat
@@ -0,0 +1,4 @@
+del ..\Java-PLSQL-Bridge-Code-Generator-X.X.zip
+"c:\Program Files\7-Zip\7z.exe" -x!Java-PLSQL-Bridge-Code-Generator\Release -x!Java-PLSQL-Bridge-Code-Generator\.git a ..\Java-PLSQL-Bridge-Code-Generator-X.X.zip ..\..\..\Java-PLSQL-Bridge-Code-Generator
+
+rem Java-PLSQL-Bridge-Code-Generator\Generator Java-PLSQL-Bridge-Code-Generator\Runtime Java-PLSQL-Bridge-Code-Generator\README.txt
