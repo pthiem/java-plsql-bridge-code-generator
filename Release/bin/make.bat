596a9f88f4313afeef8b2a162942e72e2c9146e4
Peter Thiem <peterthiem@gmail.com>
2012-01-17 17:13:39 +1100

    Improve Demo.

--- Release/bin/make.bat
+++ Release/bin/make.bat
@@ -1,2 +1,10 @@
 del ..\Java-PLSQL-Bridge-Code-Generator-X.X.zip
-"c:\Program Files\7-Zip\7z.exe" -x!Java-PLSQL-Bridge-Code-Generator\Release -x!Java-PLSQL-Bridge-Code-Generator\.git a ..\Java-PLSQL-Bridge-Code-Generator-X.X.zip ..\..\..\Java-PLSQL-Bridge-Code-Generator
+"c:\Program Files\7-Zip\7z.exe" ^
+ -x!Java-PLSQL-Bridge-Code-Generator\Release ^
+ -x!Java-PLSQL-Bridge-Code-Generator\.git ^
+ -x!Java-PLSQL-Bridge-Code-Generator\Demo\bin\CodeGenerator.properties ^
+ -x!Java-PLSQL-Bridge-Code-Generator\Demo\src\plsql_bridge.properties ^
+ -x!Java-PLSQL-Bridge-Code-Generator\Demo\classes\* ^
+ a ^
+ ..\Java-PLSQL-Bridge-Code-Generator-X.X.zip ^
+ ..\..\..\Java-PLSQL-Bridge-Code-Generator
